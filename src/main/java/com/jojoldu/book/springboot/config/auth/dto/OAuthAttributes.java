34cc265ea478af2dfc7ddb9aae6859f741d98404
Rebwon <msolo021015@naver.com>
2020-01-03 09:01:23 +0900

    로그인 정보 어노테이션 기반으로 변경 세션 정보 저장 구현, 네이버 로그인 추가, Security추가 후 깨지는 테스트 수정.

--- src/main/java/com/jojoldu/book/springboot/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/jojoldu/book/springboot/config/auth/dto/OAuthAttributes.java
@@ -27,7 +27,22 @@ public class OAuthAttributes {
 
     // OAuth2User에서 반환하는 사용자 정보는 Map이기 때문에 값 하나하나를 변환하는데 사용하는 메서드
     public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes) {
+        if("naver".equals(registrationId)) {
+            return ofNaver("id", attributes);
+        }
         return ofGoogle(userNameAttributeName, attributes);
+    }
+
+    private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes) {
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+
+        return OAuthAttributes.builder()
+                .name((String) response.get("name"))
+                .email((String) response.get("email"))
+                .picture((String) response.get("profile_image"))
+                .attributes(response)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
     }
 
     private static OAuthAttributes ofGoogle(String userNameAttributeName, Map<String, Object> attributes) {
@@ -43,12 +58,13 @@ public class OAuthAttributes {
     // User 엔티티를 생성함.
     // OAuth2Attributes에서 엔티티를 생성하는 시점은 처음 가입할 때이다.
     // 가입할 때의 기본권한을 GUEST로 주기 위해서 role 빌더 값에는 Role.GUEST를 사용.
+    // GUEST로 기본주고 USER로 바꾸는 방법을 생각하자.
     public User toEntity() {
         return User.builder()
                     .name(name)
                     .email(email)
                     .picture(picture)
-                    .role(Role.GUEST)
+                    .role(Role.USER)
                     .build();
     }
 }
