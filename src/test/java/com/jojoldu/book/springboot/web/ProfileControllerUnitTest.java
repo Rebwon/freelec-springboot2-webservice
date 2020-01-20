b4eb61307cebd52a09ba109752ba7f746d0f3185
Rebwon <msolo021015@naver.com>
2020-01-20 16:10:57 +0900

    Profile에 따른 설정 구별을 위한 테스트 및 Security 설정

---
+++ src/test/java/com/jojoldu/book/springboot/web/ProfileControllerUnitTest.java
@@ -0,0 +1,59 @@
+package com.jojoldu.book.springboot.web;
+
+import org.junit.Test;
+import org.springframework.mock.env.MockEnvironment;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ProfileControllerUnitTest {
+    @Test
+    public void real_profile이_조회된다() {
+        // given
+        String exceptedProfile = "real";
+        MockEnvironment env = new MockEnvironment();
+        env.addActiveProfile(exceptedProfile);
+        env.addActiveProfile("oauth");
+        env.addActiveProfile("real-db");
+
+        ProfileController controller = new ProfileController(env);
+
+        // when
+        String profile = controller.profile();
+
+        // then
+        assertThat(profile).isEqualTo(exceptedProfile);
+    }
+
+    @Test
+    public void real_profile이_없으면_첫번째가_조회된다() {
+        // given
+        String exceptedProfile = "oauth";
+        MockEnvironment env = new MockEnvironment();
+
+        env.addActiveProfile(exceptedProfile);
+        env.addActiveProfile("real-db");
+
+        ProfileController controller = new ProfileController(env);
+
+        // when
+        String profile = controller.profile();
+
+        // then
+        assertThat(profile).isEqualTo(exceptedProfile);
+    }
+
+    @Test
+    public void active_profile이_없으면_default가_조회된다() {
+        // given
+        String exceptedProfile = "default";
+        MockEnvironment env = new MockEnvironment();
+        ProfileController controller = new ProfileController(env);
+
+        // when
+        String profile = controller.profile();
+
+        // then
+        assertThat(profile).isEqualTo(exceptedProfile);
+    }
+
+}
